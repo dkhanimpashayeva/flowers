12bba3a1809b6bb0e5ab7cd2cc3f992fe9df61ed
khanimcode <7f3p8e2@code.edu.az>
2023-01-25 21:31:34 +0400

    bhbjsax

--- frontend/src/schema/formschema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-export const formSchema = yup.object().shape({
-  name: yup.string().required(),
-  price: yup.number().required().positive().integer(),
-
-  url: yup.string().url().required(),
-  
-});
